75abf57083ae1d35735b1521df4f10c1a126082f
Lorenzo Puchetta <lpuchetta@fi.uba.ar>
2025-04-22 17:27:35 -0300

    Agregue la estructura del iterador, la funcion para crear y las primitivas simples (VerActual, Siguiente, HaySiguiente)

--- lista/lista_enlazada.go
+++ lista/lista_enlazada.go
@@ -1,20 +1,62 @@
 package lista
 
-type NodoLista[T any] struct{
-	proximo *NodoLista[T]
+type nodoLista[T any] struct{
+	proximo *nodoLista[T]
 	dato T
 }
 
-type ListaEnlazada[T any] struct{
-	primero *NodoLista[T]
-	ultimo *NodoLista[T]
+type listaEnlazada[T any] struct{
+	primero *nodoLista[T]
+	ultimo *nodoLista[T]
 	largo int
 }
 
-func CrearListaEnlazada[T any]() *ListaEnlazada[T]{
-	return &ListaEnlazada[T]{
+type iteradorListaEnlazada[T any] struct{
+	actual *nodoLista[T]
+	anterior *nodoLista[T] 
+	lista *listaEnlazada[T]
+}
+
+func CrearListaEnlazada[T any]() listaEnlazada[T]{
+	return listaEnlazada[T]{
 		primero : nil,
 		ultimo : nil,
 		largo : 0,
 	}
 }
+
+func (l *listaEnlazada[T]) Iterador() iteradorListaEnlazada[T]{
+	return iteradorListaEnlazada[T]{
+		actual : l.primero,
+		anterior : nil,
+		lista : l,
+	}
+}
+
+func (it *iteradorListaEnlazada[T]) HaySiguiente() bool{
+	return it.actual != nil
+}
+
+func (it *iteradorListaEnlazada[T]) VerActual() T{
+	if !it.HaySiguiente(){
+		panic ("EL iterador termino de iterar")
+	}
+	return it.actual.dato	
+}
+
+func (it *iteradorListaEnlazada[T]) Siguiente(){
+	if !it.HaySiguiente(){
+		panic ("El iterador termino de iterar")
+	}
+	it.anterior = it.actual
+	it.actual = it.actual.proximo
+}
+
+func (l *listaEnlazada[T]) Iterar(visitar func(T) bool){
+	for actual := l.primero; actual != nil; actual = actual.proximo{
+		if !visitar(actual.dato){
+			break
+		}
+	}
+}
+
