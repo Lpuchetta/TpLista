f34dd4d22b7b3b126e564e764d3d15d40362a8b0
Lorenzo Puchetta <lpuchetta@fi.uba.ar>
2025-04-24 08:40:11 -0300

    Agrego casos bordes, revisar si agregar mas

--- lista/lista_test.go
+++ lista/lista_test.go
@@ -138,3 +138,59 @@ func TestVolumen(t *testing.T) {
 		lista.BorrarPrimero()
 	})
 }
+
+func TestCasosBordes(t *testing.T) {
+	lista := TDALista.CrearListaEnlazada[int]()
+	require.True(t, lista.EstaVacia())
+	require.Equal(t, 0, lista.Largo())
+	require.PanicsWithValue(t, "La lista esta vacia", func(){
+		lista.VerPrimero()
+	})
+	require.PanicsWithValue(t, "La lista esta vacia", func(){
+		lista.VerUltimo()
+	})
+	require.PanicsWithValue(t, "La lista esta vacia", func(){
+		lista.BorrarPrimero()
+	})
+	lista.InsertarPrimero(44)
+	require.Equal(t, 44, lista.VerPrimero())
+	require.Equal(t, 44, lista.VerUltimo())
+	require.Equal(t, 1, lista.Largo())
+	require.False(t, lista.EstaVacia())
+	require.Equal(t, 44, lista.BorrarPrimero())
+	require.True(t, lista.EstaVacia())
+	require.Equal(t, 0, lista.Largo())
+
+	lista.InsertarUltimo(1)
+	lista.InsertarUltimo(2)
+	require.Equal(t, 1, lista.VerPrimero())
+	require.Equal(t, 2, lista.VerUltimo())
+	require.Equal(t, 1, lista.BorrarPrimero())
+	require.Equal(t, 2, lista.BorrarPrimero())
+
+	lista.InsertarPrimero(100)
+	require.Equal(t, 100, lista.BorrarPrimero())
+	require.True(t, lista.EstaVacia())
+	require.PanicsWithValue(t, "La lista esta vacia", func() {
+		lista.VerPrimero()
+	})
+	require.PanicsWithValue(t, "La lista esta vacia", func() {
+		lista.VerUltimo()
+	})
+	require.PanicsWithValue(t, "La lista esta vacia", func(){
+		lista.BorrarPrimero()
+	})
+	
+	lista.InsertarUltimo(30)
+	require.Equal(t, 30, lista.VerUltimo())
+	require.Equal(t, 30, lista.VerPrimero())
+	require.Equal(t, 30, lista.BorrarPrimero())
+
+	lista.InsertarPrimero(7)
+	lista.InsertarPrimero(7)
+	lista.InsertarUltimo(7)
+	require.Equal(t, 3, lista.Largo())
+	require.Equal(t, 7, lista.BorrarPrimero())
+	require.Equal(t, 7, lista.BorrarPrimero())
+	require.Equal(t, 7, lista.BorrarPrimero())	
+}
