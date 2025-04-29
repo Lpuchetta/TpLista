1ededf917adba8289fab58101eb35bbe9aaa594d
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-04-29 16:43:24 -0300

    Agrego la estructura básica de las primitivas del dicc

--- diccionario/hash_abierto.go
+++ diccionario/hash_abierto.go
@@ -17,3 +17,36 @@ func CrearHash[K comparable, V any]() Diccionario[K, V] {
 		cantidad: 0,
 	}
 }
+
+func (hash *hashAbierto[K, V]) Guardar(clave K, dato V) {
+
+}
+
+func (hash *hashAbierto[K, V]) Pertenece(clave K) bool {
+	return false
+}
+
+func (hash *hashAbierto[K, V]) Obtener(clave K) V {
+	if !hash.Pertenece(clave) {
+		panic("La clave no pertenece al diccionario")
+	}
+
+}
+
+func (hash *hashAbierto[K, V]) Borrar(clave K) V {
+	if !hash.Pertenece(clave) {
+		panic("La clave no pertenece al diccionario")
+	}
+}
+
+func (hash *hashAbierto[K, V]) Cantidad() int {
+	return hash.cantidad
+}
+
+func (hash *hashAbierto[K, V]) Iterar(visitar func(K, V) bool) {
+
+}
+
+func (hash *hashAbierto[K, V]) Iterador() IterDiccionario[K, V] {
+	return nil
+}
