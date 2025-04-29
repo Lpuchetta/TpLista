58481d60b15534b201b6e9e74b060c971b01d70e
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-04-29 17:11:05 -0300

    Agrego struct parClaveValor y corrijo la impl de CrearHash

--- diccionario/hash_abierto.go
+++ diccionario/hash_abierto.go
@@ -1,19 +1,29 @@
 package diccionario
 
 import (
+	"fmt"
+	"hash/fnv"
 	TDALista "tdas/lista"
 )
 
-const _CAPACIDAD_INICIAL = 23 // Es mejor que sea un número primo.
+const (
+	_CAPACIDAD_INICIAL = 23 // Es mejor que sea un número primo.
+	_FACTOR_CARGA      = 1.2
+)
+
+type parClaveValor[K comparable, V any] struct {
+	clave K
+	dato  V
+}
 
 type hashAbierto[K comparable, V any] struct {
-	casillas []TDALista.Lista[K]
+	casillas []TDALista.Lista[parClaveValor[K, V]]
 	cantidad int
 }
 
 func CrearHash[K comparable, V any]() Diccionario[K, V] {
 	return &hashAbierto[K, V]{
-		casillas: make([]TDALista.Lista[K], _CAPACIDAD_INICIAL),
+		casillas: make([]TDALista.Lista[parClaveValor[K, V]], _CAPACIDAD_INICIAL),
 		cantidad: 0,
 	}
 }
@@ -49,4 +59,24 @@ func (hash *hashAbierto[K, V]) Iterar(visitar func(K, V) bool) {
 
 func (hash *hashAbierto[K, V]) Iterador() IterDiccionario[K, V] {
 	return nil
+}
+
+func (hash *hashAbierto[K, V]) buscar()
+
+// indexDe define en que casilla del arreglo de listas enlazadas debe caer el par clave-valor.
+func (h *hashAbierto[K, V]) indexDe(clave K) int {
+	hv := h.hashClave(clave)
+	return int(hv % uint64(len(h.casillas)))
+}
+
+// hashClave usa FNV-1a para generar un hash de la clave.
+func (h *hashAbierto[K, V]) hashClave(clave K) uint64 {
+	hf := fnv.New64a()
+	hf.Write(convertirABytes(clave))
+	return hf.Sum64()
+}
+
+// convertirABytes convierte una clave generica a []byte.
+func convertirABytes[K comparable](clave K) []byte {
+	return []byte(fmt.Sprintf("%v", clave))
 }
