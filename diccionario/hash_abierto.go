728af66e2c717f40935a7291ac8a4f14625483a1
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-04-29 16:36:09 -0300

    Agrego la impl de la función CrearHash

--- diccionario/hash_abierto.go
+++ diccionario/hash_abierto.go
@@ -1,13 +1,19 @@
 package diccionario
 
-import(
-	TDALista	"tdas/lista"
+import (
+	TDALista "tdas/lista"
 )
 
-const _CAPACIDAD_INICIAL = 16
+const _CAPACIDAD_INICIAL = 23 // Es mejor que sea un número primo.
 
-type hashAbierto[K comparable, V any] struct{
-	casillas 	[]TDALista.Lista[K]
-	cantidad	int
+type hashAbierto[K comparable, V any] struct {
+	casillas []TDALista.Lista[K]
+	cantidad int
 }
 
+func CrearHash[K comparable, V any]() Diccionario[K, V] {
+	return &hashAbierto[K, V]{
+		casillas: make([]TDALista.Lista[K], _CAPACIDAD_INICIAL),
+		cantidad: 0,
+	}
+}
