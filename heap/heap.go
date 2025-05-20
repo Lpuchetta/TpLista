1e8c35d4cc3fdcc53fbb8f0bdb2e1660dda710c7
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-05-20 08:37:54 -0300

    Agrego el cuerpo de las primitivas de la ColaPrioridad sin impl

--- heap/heap.go
+++ heap/heap.go
@@ -1,5 +1,7 @@
 package cola_prioridad
 
+const _CAPACIDAD_INICIAL = 5
+
 type colaConPrioridad[T any] struct {
 	datos []T
 	cant  int
@@ -7,9 +9,33 @@ type colaConPrioridad[T any] struct {
 }
 
 func CrearHeap[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
-	return nil
+	return &colaConPrioridad[T]{
+		datos: make([]T, _CAPACIDAD_INICIAL),
+		cant:  0,
+		cmp:   funcion_cmp,
+	}
 }
 
 func CrearHeapArr[T any](arreglo []T, funcion_cmp func(T, T) int) ColaPrioridad[T] {
 	return nil
+}
+
+func (cola *colaConPrioridad[T]) EstaVacia() bool {
+	return false
+}
+
+func (cola *colaConPrioridad[T]) Encolar(dato T) {
+
+}
+
+func (cola *colaConPrioridad[T]) VerMax() T {
+
+}
+
+func (cola *colaConPrioridad[T]) Desencolar() T {
+
+}
+
+func (cola *colaConPrioridad[T]) Cantidad() int {
+	return cola.cant
 }
